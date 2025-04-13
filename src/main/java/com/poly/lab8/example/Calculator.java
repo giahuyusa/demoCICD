b0272ee1b7730dcd38f91c90dc1bdbe195b07a19
giahuyusa <giahuy172005@gmail.com>
2025-04-13 13:35:20 +0700

    update Calculator

--- src/main/java/com/poly/lab8/example/Calculator.java
+++ src/main/java/com/poly/lab8/example/Calculator.java
@@ -2,9 +2,9 @@ package com.poly.lab8.example;
 
 public class Calculator implements Icaculator{
     public Integer sum (Integer a, Integer b){
-        return 0;
+        return a + b;
     }
     public Integer sub (Integer a, Integer b){
-        return 0;
+        return a - b ;
     }
 }
