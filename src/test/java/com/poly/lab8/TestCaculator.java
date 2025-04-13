a0cbf849ddc62e3978a5551c464307f15133d829
giahuyusa <giahuy172005@gmail.com>
2025-04-13 12:38:18 +0700

    test fail

---
+++ src/test/java/com/poly/lab8/TestCaculator.java
@@ -0,0 +1,21 @@
+package com.poly.lab8;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.poly.lab8.example.Calculator;
+
+public class TestCaculator {
+
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(calculator.sum(2, 3), 5);
+    }
+
+    @Test
+    public void testSub() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(calculator.sub(2, 2), 0);
+    }
+}
