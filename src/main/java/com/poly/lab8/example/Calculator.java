82d84c847d8e886609350a78f1d79d847204a8fd
giahuyusa <giahuy172005@gmail.com>
2025-04-13 12:01:30 +0700

    First commit

---
+++ src/main/java/com/poly/lab8/example/Calculator.java
@@ -0,0 +1,10 @@
+package com.poly.lab8.example;
+
+public class Calculator implements Icaculator{
+    public Integer sum (Integer a, Integer b){
+        return 0;
+    }
+    public Integer sub (Integer a, Integer b){
+        return 0;
+    }
+}
---
+++ src/main/java/com/poly/lab8/example/Icaculator.java
@@ -0,0 +1,6 @@
+package com.poly.lab8.example;
+
+public interface Icaculator {
+Integer sum(Integer a, Integer b);
+Integer sub(Integer a, Integer b);
+}
