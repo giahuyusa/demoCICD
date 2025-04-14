f595374ef2248f46907cf0318d1cfe39548722a7
giahuyusa <giahuy172005@gmail.com>
2025-04-14 09:55:06 +0700

    test fail

--- src/main/java/com/poly/lab8/example/Calculator.java
+++ src/main/java/com/poly/lab8/example/Calculator.java
@@ -2,9 +2,9 @@ package com.poly.lab8.example;
 
 public class Calculator implements Icaculator{
     public Integer sum (Integer a, Integer b){
-        return a + b;
+        return 0;
     }
     public Integer sub (Integer a, Integer b){
-        return a - b ;
+        return 0;
     }
 }
